8a8a899450765251cea9cf51908d49cc4f9f7812
manhung99 <manhung99@users.noreply.github.com>
2018-10-20 21:55:29 +0700

    update

--- src/Data/DatabaseConnection.java
+++ src/Data/DatabaseConnection.java
@@ -1,12 +1,16 @@
 package data;
 
+import java.io.File;
 import java.sql.*;
 
 public class DatabaseConnection {
     public static Connection getConnection() {
         try {
+            String path = new File("").getAbsolutePath();
+            path = path.concat("/src/data/dict_hh.db");
+
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection("jdbc:sqlite:C:\\Github\\asg1-100-team-1\\src\\data\\dict_hh.db");
+            return DriverManager.getConnection("jdbc:sqlite:" + path);
         } catch (Exception e) {
             System.out.println(e);
             return null;
